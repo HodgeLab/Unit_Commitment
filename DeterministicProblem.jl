975950499b19b3e1c0dd562705be30a54838b3e5
Doubleday <kdoubled@nrel.gov>
2021-05-06 17:53:04 -0600

    Extract repeated manual updates to own function

--- DeterministicProblem.jl
+++ DeterministicProblem.jl
@@ -23,43 +23,7 @@ system_da = System(joinpath(system_file_path, "DA_sys.json"); time_series_read_o
 
 # Jose's tune-ups for the HA UC
 for system in [system_da] # [system_da, system_ha, system_ed]
-    for g in get_components(
-        ThermalMultiStart,
-        system,
-        x -> get_prime_mover(x) == ThermalFuels.NATURAL_GAS,
-    )
-        lims = get_ramp_limits(g)
-        set_time_limits!(g, (up = 1.1 * lims.up, down = 1.1 * lims.down))
-        set_ramp_limits!(g, (up = 1.25 * lims.up, down = 1.25 * lims.down))
-    end
-
-    for g in get_components(
-        ThermalMultiStart,
-        system,
-        x -> get_prime_mover(x) == ThermalFuels.COAL,
-    )
-        lims = get_ramp_limits(g)
-        set_ramp_limits!(g, (up = 1.25 * lims.up, down = 1.25 * lims.down))
-    end
-    for g in get_components(
-        RenewableDispatch,
-        system,
-        x -> get_prime_mover(x) != PrimeMovers.PVe,
-    )
-        set_available!(g, true)
-    end
-
-    for g in get_components(HydroGen, system)
-        set_available!(g, true)
-    end
-
-    s = get_component(VariableReserve{ReserveUp}, system, "REG_UP")
-    req = get_requirement(s)
-    set_requirement!(s, req * 1.5)
-
-    s = get_component(VariableReserve{ReserveDown}, system, "REG_DN")
-    req = get_requirement(s)
-    set_requirement!(s, req * 1.5)
+    appply_manual_data_updates!(system)
 end
 
 # Set all CC's to start off
