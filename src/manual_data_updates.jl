578127d6bd0be67d47b993e8166f2e2d848a3991
Doubleday <kdoubled@nrel.gov>
2021-05-19 16:00:31 -0600

    Move CC initial turn-off within manual updates function

--- src/manual_data_updates.jl
+++ src/manual_data_updates.jl
@@ -10,4 +10,18 @@ function appply_manual_data_updates!(system)
     for g in get_components(HydroGen, system)
         set_available!(g, true)
     end
+
+    # Set all CC's to start off
+    for g in get_components(
+        ThermalMultiStart,
+        system,
+        x -> get_prime_mover(x) in [PrimeMovers.CT, PrimeMovers.CC],
+    )
+        set_status!(g, false)
+        set_active_power!(g, 0.0)
+    end
+
+    # g = get_component(ThermalMultiStart, system, "SANDY_CREEK_J04")
+    # set_status!(g, false)
+    # set_active_power!(g, 0.0)
 end
