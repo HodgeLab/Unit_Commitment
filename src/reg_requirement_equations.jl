47f537e55afb09b8e0a856c0093289afd64e31db
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-25 18:17:31 -0700

    fix typo

--- src/reg_requirement_equations.jl
+++ src/reg_requirement_equations.jl
@@ -26,7 +26,7 @@ function apply_reg_requirements!(
     # These also need registration in the optimization Containers
 
     optimization_container.variables[:REG_UP__VariableReserve_ReserveUp] = reg⁺ = obj_dict[:reg⁺]
-    optimization_container.variables[:REG_UP__VariableReserve_ReserveDown] = reg⁻ = obj_dict[:reg⁻]
+    optimization_container.variables[:REG_DN__VariableReserve_ReserveDown] = reg⁻ = obj_dict[:reg⁻]
     if use_solar_reg
         reg⁺_S = obj_dict[:reg⁺_S]
         reg⁻_S = obj_dict[:reg⁻_S]
